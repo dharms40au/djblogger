8357347b8d7e7196bab47602ac907d180c09d814
Dave Harms <dave.harms@fortyau.com>
2023-05-27 10:13:43 -0500

    added blog project

--- djblogger/djblogger/settings/base.py
+++ djblogger/djblogger/settings/base.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 from dotenv import load_dotenv
+
+
 import os
 
 load_dotenv()
@@ -40,6 +42,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "djblogger.blog",
 ]
 
 MIDDLEWARE = [
